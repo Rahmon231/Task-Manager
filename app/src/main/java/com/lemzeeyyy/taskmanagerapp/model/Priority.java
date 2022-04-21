adb9ba196658eef54a019df21eb5048466b2b955
Akanbi Rahmon <Rahmon.Akanbi@outlook.com>
2022-04-21 15:13:56 +0100

    Task entity setup

---
+++ app/src/main/java/com/lemzeeyyy/taskmanagerapp/model/Priority.java
@@ -0,0 +1,7 @@
+package com.lemzeeyyy.taskmanagerapp.model;
+
+public enum Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ app/src/main/java/com/lemzeeyyy/taskmanagerapp/model/Task.java
@@ -0,0 +1,83 @@
+package com.lemzeeyyy.taskmanagerapp.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity(tableName = "task_table")
+public class Task {
+    @ColumnInfo(name = "task_id")
+    @PrimaryKey(autoGenerate = true)
+    public long taskId;
+
+    public Priority priority;
+
+    @ColumnInfo(name = "due_date")
+    public Date dueDate;
+
+    @ColumnInfo(name = "date_created")
+    public Date dateCreated;
+
+    @ColumnInfo(name = "is_done")
+    public Boolean isDone;
+
+    public Task(Priority priority, Date dueDate, Date dateCreated, Boolean isDone) {
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.dateCreated = dateCreated;
+        this.isDone = isDone;
+    }
+
+    public long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "taskId=" + taskId +
+                ", priority=" + priority +
+                ", dueDate=" + dueDate +
+                ", dateCreated=" + dateCreated +
+                ", isDone=" + isDone +
+                '}';
+    }
+}
